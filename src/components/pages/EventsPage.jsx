e6c5a8106c4c5d7427ee138fa4f6c2a697f7d239
Martijns3 <spierenburg2480@gmail.com>
2023-09-04 09:12:43 +0200

    sorting fix

--- src/components/pages/EventsPage.jsx
+++ src/components/pages/EventsPage.jsx
@@ -3,8 +3,8 @@ import { EventCard } from "../EventCard";
 
 export const EventsPage = ({ matchedEvents }) => {
     matchedEvents.sort(function (a, b) {
-        var keyA = new Date(a.startTime.split("T")[0]),
-            keyB = new Date(b.startTime.split("T")[0]);
+        var keyA = new Date(a.startTime),
+            keyB = new Date(b.startTime);
         if (keyA < keyB) return -1;
         if (keyA > keyB) return 1;
         return 0;
@@ -13,7 +13,6 @@ export const EventsPage = ({ matchedEvents }) => {
         <Flex
             direction="column"
             align="center"
-            // flexWrap="wrap"
             paddingTop={8}
             paddingBottom={6}
             gap={7}
